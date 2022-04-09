2acde5a85824b2f85d13370d6579da7791deed73
xCall <carlosfaita99@gmail.com>
2022-04-09 12:50:13 -0300

    Adicionado novo test

--- __tests__/middlewares/user.test.js
+++ __tests__/middlewares/user.test.js
@@ -1,15 +1,15 @@
 import { isAdminMiddleware } from 'middlewares/user';
- import ROLES from 'constants/roles';
+import ROLES from 'constants/roles';
 
- const mockUsuario = {
-   uid: 'abc-1234',
-   userName: 'nomeDoUsuario',
-   name: 'nome',
-   email: 'email.nome@usuario.com',
-   password: 'senhasupersecreta',
- };
+const mockUsuario = {
+  uid: 'abc-1234',
+  userName: 'nomeDoUsuario',
+  name: 'nome',
+  email: 'email.nome@usuario.com',
+  password: 'senhasupersecreta',
+};
 
- it('Deve retornar os dados do usuário caso a role seja ADMIN', () =>{
+it('Deve retornar os dados do usuário caso a role seja ADMIN', () => {
   const mockAdmin = {
     user: {
       ...mockUsuario,
@@ -19,4 +19,16 @@ import { isAdminMiddleware } from 'middlewares/user';
 
   const retorno = isAdminMiddleware(mockAdmin);
   expect(retorno).toEqual(mockAdmin);
- });
+});
+
+it('Deve disparar um erro caso o usuário não seja ADMIN', () => {
+  const mockUser = {
+    user: {
+      ...mockUsuario,
+      role: ROLES.USER,
+    },
+  };
+  const retorno = () => isAdminMiddleware(mockUser);
+  expect(retorno).toThrow('Você não possui permissão para executar essa operação.');
+  
+});
